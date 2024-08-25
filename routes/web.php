ea9ecaa90bf042cb4d8aa0f58e243d6d795262c9
diyahay <youngay177@gmail.com>
2024-08-25 17:24:23 +0700

    #1 pembuatan form dan aksi login

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,18 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ProductController;
+use app\Http\Controllers\ProductController;
+use App\Http\Controllers\Auth\LoginRegisterController;
+
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
 
-Route::resource('products', ProductController::class);
+Route::controller(LoginRegisterController::class)->group(function() {
+    Route::get('/register', 'register')->name('register');
+    Route::post('/store', 'store')->name('store');
+    Route::get('/login', 'login')->name('login');
+    Route::post('/authenticate', 'authenticate')->name('authenticate');
+    Route::get('/home', 'home')->name('home');
+    Route::post('/logout', 'logout')->name('logout');
+});
